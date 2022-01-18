ff903c211f4ad1f9488bda63d581e4fb7b8dffaf
Tapio Naumanen <tapio.naumanen@visma.com>
2022-01-18 17:11:46 +0200

    Add correct provider for acm-certificate and acm-certificate-validation modules

--- infrastructure/modules/networking/acm-certificate/main.tf
+++ infrastructure/modules/networking/acm-certificate/main.tf
@@ -1,4 +1,5 @@
 resource "aws_acm_certificate" "cert" {
+  provider                  = aws.us-east-1
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"

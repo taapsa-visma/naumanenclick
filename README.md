a2cfed2ecf1f7f56af7fd0f340ab0646185f3e54
Tapio Naumanen <tapio.naumanen@visma.com>
2022-01-20 17:37:14 +0200

    Adding react site

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # naumanen.click
+
 Everything for naumanen.click website
+
+## Next steps
+
+1. New project created with typescript create-react-app, customize the default app a little.
+   1. Check out create-react-apps vulnerabilities
+   2. Check out Reacts debugging and hot reload
+2. Build a Github pipeline, so that when building the production build it is uploaded to S3 bucket and Cloudfront invalidates cache (and automatically gets the latest from S3)
---
+++ site/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>naumanen.click</h1>
+      </header>
+      <div className="App-content">
+        <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;

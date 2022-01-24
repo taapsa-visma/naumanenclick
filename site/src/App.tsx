184475aa98b1428c69f3a26d58b656b437faf31e
Tapio Naumanen <tapio.naumanen@visma.com>
2022-01-24 16:44:52 +0200

    Little change to check deployment speed

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <div data-testid="content" className="App-content">
         <code>Hello World</code>
         <p>
-          You should probably learn <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a> ¯\_(ツ)_/¯ ?
+          You should learn <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a> ¯\_(ツ)_/¯ ?
         </p>
       </div>
     </div>

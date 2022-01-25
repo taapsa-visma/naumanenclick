0330e361a700b97430c41b8d1e8d6735e15f310f
Tapio Naumanen <tapio.naumanen@visma.com>
2022-01-25 22:35:31 +0200

    emailaddress class and use it in the react app

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Everything for naumanen.click website
 
 [![GitHub Super-Linter](https://github.com/taapsa-visma/naumanenclick/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
+
+## DEV stuff
+
+When developing modules do `yarn link` in your module folder. Then go to main app folder and run `yarn link <module name>` and you are all set. When ever you do changes just build the module and start the app again.
--- site/common/src/index.ts
+++ site/common/src/index.ts
@@ -1,3 +1,13 @@
-export function sayHello(name: string) {
-    return `Hey ${name}, say hello to TypeScript!`;
-}
+export class EmailAddress {
+    public static isValid(email: string | null | undefined): boolean {
+      if (!email) {
+        return false;
+      }
+  
+      const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+      return regexp.test(email.toLowerCase());
+    }
+  }
+
+  
